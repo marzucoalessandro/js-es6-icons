bcdba1dfcc4aabefb5ea4904dbe83c3cfd2e6821
Alessandro <marzucoalessandro1@gmail.com>
2020-12-10 17:34:54 +0100

    milestone 1

--- milestone1.js
+++ milestone1.js
@@ -4,7 +4,7 @@ $(document).ready(function () {
      {
       name: "cat",
       prefix: `  <div>
-        <i class="fas fa-cat" style="color:blue"></i>
+        <i class="fas fa-cat"></i>
         <div class="title">CAT</div>
       </div>`,
       type: "",
@@ -13,7 +13,7 @@ $(document).ready(function () {
     {
       name: "crow",
       prefix:  `  <div>
-        <i class="fas fa-crow" style="color:blue"></i>
+        <i class="fas fa-crow"></i>
         <div class="title">CROW</div>
       </div>`,
       type: "",
@@ -22,7 +22,7 @@ $(document).ready(function () {
     {
       name: "dog",
       prefix:  `  <div>
-        <i class="fas fa-dog" style="color:blue"></i>
+        <i class="fas fa-dog"></i>
         <div class="title">DOG</div>
       </div>`,
       type: "",
@@ -31,7 +31,7 @@ $(document).ready(function () {
     {
       name: "dove",
       prefix:  `  <div>
-        <i class="fas fa-dove" style="color:blue"></i>
+        <i class="fas fa-dove"></i>
         <div class="title">DOVE</div>
       </div>`,
       type: "",
@@ -40,7 +40,7 @@ $(document).ready(function () {
     {
       name: "dragon",
       prefix: `  <div>
-        <i class="fas fa-dragon" style="color:blue"></i>
+        <i class="fas fa-dragon"></i>
         <div class="title">DRAGON</div>
       </div>`,
       type: "",
@@ -49,7 +49,7 @@ $(document).ready(function () {
     {
       name: "horse",
       prefix: `  <div>
-        <i class="fas fa-horse" style="color:blue"></i>
+        <i class="fas fa-horse"></i>
         <div class="title">HORSE</div>
       </div>`,
       type: "",
@@ -58,7 +58,7 @@ $(document).ready(function () {
     {
       name: "hippo",
       prefix: `  <div>
-        <i class="fas fa-hippo" style="color:blue"></i>
+        <i class="fas fa-hippo"></i>
         <div class="title">HIPPO</div>
       </div>`,
       type: "",
@@ -67,12 +67,84 @@ $(document).ready(function () {
     {
       name: "fish",
       prefix: `  <div>
-        <i class="fas fa-fish" style="color:blue"></i>
+        <i class="fas fa-fish"></i>
         <div class="title">FISH</div>
       </div>`,
       type: "",
       family: "animal"
-    }
+    },
+    {
+      name: "carrot",
+      prefix:  `  <div>
+        <i class="fas fa-carrot"></i>
+        <div class="title">CARROT</div>
+      </div>`,
+      tipe: "",
+      family: "vegetables"
+    },
+    {
+      name: "apple",
+      prefix:  `  <div>
+        <i class="fas fa-apple-alt"></i>
+        <div class="title">APPLE-ALT</div>
+      </div>`,
+      tipe: "",
+      family: "vegetables"
+    },
+    {
+      name: "lemon",
+      prefix:  `  <div>
+        <i class="fas fa-lemon"></i>
+        <div class="title">LEMON</div>
+      </div>`,
+      tipe: "",
+      family: "vegetables"
+    },
+    {
+      name: "pepper",
+      prefix:  `  <div>
+        <i class="fas fa-pepper-hot"></i>
+        <div class="title">PEPPER-HOT</div>
+      </div>`,
+      tipe: "",
+      family: "vegetables"
+    },
+    {
+      name: "astronaut",
+      prefix: `  <div>
+        <i class="fas fa-user-astronaut"></i>
+        <div class="title">USER-ASTRONAUT</div>
+      </div>`,
+      type: "",
+      family: "people"
+    },
+    {
+      name: "graduate",
+      prefix: `  <div>
+        <i class="fas fa-user-graduate"></i>
+        <div class="title">USER-GRADUATE</div>
+      </div>`,
+      type: "",
+      family: "people"
+    },
+    {
+      name: "ninja",
+      prefix: `  <div>
+        <i class="fas fa-user-ninja"></i>
+        <div class="title">USER-NINJA</div>
+      </div>`,
+      type: "",
+      family: "people"
+    },
+    {
+      name: "secret-agent",
+      prefix: `  <div>
+        <i class="fas fa-user-secret"></i>
+        <div class="title">USER-SECRET</div>
+      </div>`,
+      type: "",
+      family: "people"
+    },
   ];
 
   console.log(iconsList);
