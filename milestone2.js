7cd2bc16dc9dd2f77e44324850a6f679a9829c44
Alessandro <marzucoalessandro1@gmail.com>
2020-12-10 17:51:13 +0100

    inizio milestone 2

---
+++ milestone2.js
@@ -0,0 +1,171 @@
+$(document).ready(function () {
+  // Qui le icone che abbiamo definito nella milestone 1
+  const iconsList = [
+     {
+      name: "cat",
+      prefix: `  <div>
+        <i class="fas fa-cat"></i>
+        <div class="title">CAT</div>
+      </div>`,
+      type: "",
+      family: 'animal'
+    },
+    {
+      name: "crow",
+      prefix:  `  <div>
+        <i class="fas fa-crow"></i>
+        <div class="title">CROW</div>
+      </div>`,
+      type: "",
+      family: "animal"
+    },
+    {
+      name: "dog",
+      prefix:  `  <div>
+        <i class="fas fa-dog"></i>
+        <div class="title">DOG</div>
+      </div>`,
+      type: "",
+      family: "animal"
+    },
+    {
+      name: "dove",
+      prefix:  `  <div>
+        <i class="fas fa-dove"></i>
+        <div class="title">DOVE</div>
+      </div>`,
+      type: "",
+      family: "animal"
+    },
+    {
+      name: "dragon",
+      prefix: `  <div>
+        <i class="fas fa-dragon"></i>
+        <div class="title">DRAGON</div>
+      </div>`,
+      type: "",
+      family: "animal"
+    },
+    {
+      name: "horse",
+      prefix: `  <div>
+        <i class="fas fa-horse"></i>
+        <div class="title">HORSE</div>
+      </div>`,
+      type: "",
+      family: "animal"
+    },
+    {
+      name: "hippo",
+      prefix: `  <div>
+        <i class="fas fa-hippo"></i>
+        <div class="title">HIPPO</div>
+      </div>`,
+      type: "",
+      family: "animal"
+    },
+    {
+      name: "fish",
+      prefix: `  <div>
+        <i class="fas fa-fish"></i>
+        <div class="title">FISH</div>
+      </div>`,
+      type: "",
+      family: "animal"
+    },
+    {
+      name: "carrot",
+      prefix:  `  <div>
+        <i class="fas fa-carrot"></i>
+        <div class="title">CARROT</div>
+      </div>`,
+      tipe: "",
+      family: "vegetables"
+    },
+    {
+      name: "apple",
+      prefix:  `  <div>
+        <i class="fas fa-apple-alt"></i>
+        <div class="title">APPLE-ALT</div>
+      </div>`,
+      tipe: "",
+      family: "vegetables"
+    },
+    {
+      name: "lemon",
+      prefix:  `  <div>
+        <i class="fas fa-lemon"></i>
+        <div class="title">LEMON</div>
+      </div>`,
+      tipe: "",
+      family: "vegetables"
+    },
+    {
+      name: "pepper",
+      prefix:  `  <div>
+        <i class="fas fa-pepper-hot"></i>
+        <div class="title">PEPPER-HOT</div>
+      </div>`,
+      tipe: "",
+      family: "vegetables"
+    },
+    {
+      name: "astronaut",
+      prefix: `  <div>
+        <i class="fas fa-user-astronaut"></i>
+        <div class="title">USER-ASTRONAUT</div>
+      </div>`,
+      type: "",
+      family: "people"
+    },
+    {
+      name: "graduate",
+      prefix: `  <div>
+        <i class="fas fa-user-graduate"></i>
+        <div class="title">USER-GRADUATE</div>
+      </div>`,
+      type: "",
+      family: "people"
+    },
+    {
+      name: "ninja",
+      prefix: `  <div>
+        <i class="fas fa-user-ninja"></i>
+        <div class="title">USER-NINJA</div>
+      </div>`,
+      type: "",
+      family: "people"
+    },
+    {
+      name: "secret-agent",
+      prefix: `  <div>
+        <i class="fas fa-user-secret"></i>
+        <div class="title">USER-SECRET</div>
+      </div>`,
+      type: "",
+      family: "people"
+    },
+  ];
+  // definiamo dei colori per le icone (blue, orange, purple)
+  const blue = "#0084ff";
+  const orange = "#ffa500";
+  const purple = "#800080";
+
+  //aggiungiamo dei colori usando una funzione
+
+
+  //inseriamo le icone colorate nel container
+
+});
+
+
+/* ---- FUNCTIONS ----*/
+//1. Funzione milestone 1
+
+
+//2. funzione per assegnare un colore ad un icona
+// tip: possiamo invocare qui dentro un'altra funzione (2) oppure usare map
+
+
+//3. funzione per ottenere da un array
+// tip: possiamo usarla nella funzione precedente oppure usare map senza creare questa funzione.
